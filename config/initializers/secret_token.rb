8a187d2d75285ea30c589f2c289dcd61f880b7d7
enbake <enbake@ubuntu.(none)>
2013-06-15 13:21:46 +0530

    My first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App123::Application.config.secret_token = 'b44d70bdeb2a25073f107f088e31a9b3448fe48d0dbf514328339d000f8915faa3235d7eccad5df49a15437a02ac0f8279fa402a74401fa259f4a09519ce6e91'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+App123::Application.config.session_store :cookie_store, key: '_app_123_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# App123::Application.config.session_store :active_record_store
